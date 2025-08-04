4df356274f0d23402aac8064e535ed5f21d7d05f
Khushi-var10 <nehavar1018@gmail.com>
2025-08-04 14:52:18 +0530

    Find even or odd number

---
+++ b.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("Please enter any number:");
+    scanf("%d", &number);
+
+    if (number > 10)
+
+    {
+        if (number > 0)
+        {
+            if (number % 2 == 0)
+            {
+                printf("This is Even Number: ");
+            }
+            else
+            {
+                printf("This is Odd Number: ");
+            }
+        }
+        else
+        {
+            printf("Please enter valid number");
+        }
+    }
+    return 0;
+}
